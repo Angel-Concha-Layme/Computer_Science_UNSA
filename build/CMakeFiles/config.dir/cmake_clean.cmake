c107dd0ed61813aaf22a74b41b661b102a179d2d
Angel Concha Layme <aconchal@unsa.edu.pe>
2021-11-18 09:17:42 -0500

    Update CMake

---
+++ build/CMakeFiles/config.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config.dir/main.cpp.obj"
+  "CMakeFiles/config.dir/main.cpp.obj.d"
+  "config.exe"
+  "config.exe.manifest"
+  "config.pdb"
+  "libconfig.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aconc/Desktop/Computer_Science_UNSA/Compiladores/Proyecto final
+# Build directory: C:/Users/aconc/Desktop/Computer_Science_UNSA/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
