de06b80e31a6abe7872dbfc2de7246250a19bae8
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-01-04 02:30:51 -0500

    delete changes

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/aconc/Desktop/Computer_Science_UNSA/Compiladores/Proyecto final
-# Build directory: C:/Users/aconc/Desktop/Computer_Science_UNSA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
