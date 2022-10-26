3366215a3073e2adcdb1161edfdf55b316780188
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-10-26 02:52:38 -0500

    Update read_pem.py

--- Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/read_pem.py
+++ Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/read_pem.py
@@ -1,18 +1,19 @@
+from operator import contains
+import re
+from webbrowser import get
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-
+from cryptography.hazmat.primitives import hashes
+import ssl
         
 
-def get_name(certificado):
-    for i in certificado.subject.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME):
+def get_name(cert):
+    for i in cert.subject.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME):
         return i.value
 
-def get_sha(certificado): 
-    return hex(certificado.serial_number).upper()
 
-
-def get_public_key_algorithm_format(certificado):
-    format =  certificado.signature_hash_algorithm.name + ' ' +str(certificado.public_key().key_size) + ' bits'
+def get_public_key_algorithm_format(cert):
+    format =  cert.signature_hash_algorithm.name + ' ' +str(cert.public_key().key_size) + ' bits'
     return format
 
 def format_date(date):
@@ -43,28 +44,62 @@ def month_number(month):
     return months[month]
 
 
+def get_key_usage(cert):
+    usage =""
+    try:
+        cert.extensions.get_extension_for_class(x509.KeyUsage)
+    except:
+        usage = 'UNDENTIFIED'
+        return usage
+    
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.digital_signature == True:
+        usage = usage + 'digital_signature, '
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.content_commitment == True:
+        usage = usage + 'content_commitment, '
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.key_encipherment == True:
+        usage = usage + 'key_encipherment, ' 
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.data_encipherment == True:
+        usage = usage + 'data_encipherment, '
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.key_agreement == True:
+        usage = usage + 'key_agreement, '
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.key_cert_sign == True:
+        usage = usage + 'key_cert_sign, '
+    if cert.extensions.get_extension_for_class(x509.KeyUsage).value.crl_sign == True:
+        usage = usage + 'crl_sign, '
+    return usage
+
+
+
+
+
+
 def read_pem_certificates(file):
     certs_array = []
     with open(file, 'r') as f:
         certs = f.read()
         certs = certs.split('-----END CERTIFICATE-----')
         certs.pop()
+        contador = 0
         for cert in certs:
             cert = cert + '-----END CERTIFICATE-----'
             cert = x509.load_pem_x509_certificate(cert.encode(), default_backend())
 
             name = get_name(cert)
-            SHA = get_sha(cert)
-
             Public_Key_Algorithm_format = get_public_key_algorithm_format(cert)
+
+
+            key_usage =get_key_usage(cert)
+
 
             cert_dict = {
                 "Common name": name,
                 "valid_before": cert.not_valid_before.strftime("%Y-%m-%d"),
                 "valid_after": cert.not_valid_after.strftime("%Y-%m-%d"),
                 "Public Key Algorithm": Public_Key_Algorithm_format,
-                "SHA-1": SHA
+                "Key Usage": key_usage,
+                "SHA-1": (':'.join(cert.fingerprint(hashes.SHA1()).hex().upper()[i:i+2] for i in range(0, len(cert.fingerprint(hashes.SHA1()).hex().upper()), 2))),
             }
+
             certs_array.append(cert_dict)
     return certs_array
 
@@ -81,14 +116,35 @@ def read_csv_certificates(file):
                 "valid_before": valid_before,
                 "valid_after": valid_after,
                 "Public Key Algorithm": line[6],
-                "SHA-1": line[2]
+                "Key Usage": line[8].replace(';', ','),
+                "SHA-1": (':'.join(line[2].upper()[i:i+2] for i in range(0, len(line[2].upper()), 2))),
+                "Serial Number": 'NULL'
             }
             certs_array.append(cert_dict)
     return certs_array
 
 
-Microsoft_Edge = read_csv_certificates("D:/UNSA/REPOSITORIO/Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/Microsoft_edge.csv")
+#Microsoft_Edge = read_csv_certificates("D:/UNSA/REPOSITORIO/Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/Microsoft_edge.csv")
 Mozilla_Firefox = read_pem_certificates("D:/UNSA/REPOSITORIO/Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/Mozilla_Firefox.pem")
-Google_Chrome = read_pem_certificates("D:/UNSA/REPOSITORIO/Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/Google_Chrome.pem")
+#Google_Chrome = read_pem_certificates("D:/UNSA/REPOSITORIO/Topicos en Ciberseguridad/Laboratorios/Laboratorio 03/Google_Chrome.pem")
 
 
+
+
+
+def get_ssl_certificate(host, port):
+    cert = ssl.get_server_certificate((host, port))
+    cert = x509.load_pem_x509_certificate(cert.encode(), default_backend())
+    return cert
+
+
+#usar get_ssl_certificate 
+#ejemplo
+
+cert = get_ssl_certificate('www.unsa.edu.pe', 443)
+print(cert.signature_hash_algorithm)
+
+
+for cert in Mozilla_Firefox:
+    print(cert)
+    print ('\n')
